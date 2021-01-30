0ab4246dec2bc82479bfa68d923ccc21610b29f0
Jack Malone <c00236428@itcarlow.ie>
2021-01-30 13:05:44 +0000

    fixed movement

--- Crazy_Mind_f-k_Maze/InputManager.cpp
+++ Crazy_Mind_f-k_Maze/InputManager.cpp
@@ -26,6 +26,26 @@ void InputManager::update(sf::Event& t_event)
 	m_moveDown = t_event.KeyPressed && (t_event.key.code == sf::Keyboard::S || t_event.key.code == sf::Keyboard::Down);
 	m_moveLeft = t_event.KeyPressed && (t_event.key.code == sf::Keyboard::A || t_event.key.code == sf::Keyboard::Left);
 	m_moveRight = t_event.KeyPressed && (t_event.key.code == sf::Keyboard::D || t_event.key.code == sf::Keyboard::Right);
+
+	if (t_event.type == sf::Event::KeyReleased)
+	{
+		if (t_event.key.code == sf::Keyboard::W || t_event.key.code == sf::Keyboard::Up)
+		{
+			m_moveUp = false;
+		}
+		if (t_event.key.code == sf::Keyboard::A || t_event.key.code == sf::Keyboard::Left)
+		{
+			m_moveLeft = false;
+		}
+		if (t_event.key.code == sf::Keyboard::D || t_event.key.code == sf::Keyboard::Right)
+		{
+			m_moveRight = false;
+		}
+		if (t_event.key.code == sf::Keyboard::S || t_event.key.code == sf::Keyboard::Down)
+		{
+			m_moveDown = false;
+		}
+	}
 }
 
 InputManager::InputManager() : m_moveDown{ false },
