e03eae21787024d4c072ed091f12558991e25c52
Caroline Percy <lineypercy@me.com>
2021-01-30 17:08:48 +0000

    Added rotation and jumping (jumping has placeholders)

--- Crazy_Mind_f-k_Maze/InputManager.cpp
+++ Crazy_Mind_f-k_Maze/InputManager.cpp
@@ -25,38 +25,59 @@ void InputManager::update(sf::Event& t_event)
 	controller.update(); 
 
 	m_moveUp = controller.m_currentState.leftThumbStick.y > 0 || (t_event.KeyPressed && 
-		(t_event.key.code == sf::Keyboard::W || t_event.key.code == sf::Keyboard::Up));
+		(t_event.key.code == sf::Keyboard::W));
 
 	m_moveDown = controller.m_currentState.leftThumbStick.y < 0 || (t_event.KeyPressed &&
-		(t_event.key.code == sf::Keyboard::S || t_event.key.code == sf::Keyboard::Down));
+		(t_event.key.code == sf::Keyboard::S));
 
 	m_moveLeft = controller.m_currentState.leftThumbStick.x < 0 || (t_event.KeyPressed &&
-		(t_event.key.code == sf::Keyboard::A || t_event.key.code == sf::Keyboard::Left));
+		(t_event.key.code == sf::Keyboard::A));
 
 	m_moveRight = controller.m_currentState.leftThumbStick.x > 0 || (t_event.KeyPressed &&
-		(t_event.key.code == sf::Keyboard::D || t_event.key.code == sf::Keyboard::Right));
+		(t_event.key.code == sf::Keyboard::D));
+
+	m_rotateLeft = controller.m_currentState.leftB || (t_event.KeyPressed &&
+		(t_event.key.code == sf::Keyboard::Q));
+
+	m_rotateRight = controller.m_currentState.rightB || (t_event.KeyPressed &&
+		(t_event.key.code == sf::Keyboard::E));
+
+	m_jumping = (controller.m_currentState.a || (t_event.KeyPressed &&
+		(t_event.key.code == sf::Keyboard::Space))) && ! m_jumping;
+
+	if (m_jumping)
+	{
+		playersYPos = 60; // PLACEHOLDER - please find a way to add the players current y position here!
+	}
 
 
 	if (t_event.type == sf::Event::KeyReleased)
 	{
-		if (t_event.key.code == sf::Keyboard::W || t_event.key.code == sf::Keyboard::Up)
+		if (t_event.key.code == sf::Keyboard::W)
 		{
 			m_moveUp = false;
 		}
-		if (t_event.key.code == sf::Keyboard::A || t_event.key.code == sf::Keyboard::Left)
+		if (t_event.key.code == sf::Keyboard::A)
 		{
 			m_moveLeft = false;
 		}
-		if (t_event.key.code == sf::Keyboard::D || t_event.key.code == sf::Keyboard::Right)
+		if (t_event.key.code == sf::Keyboard::D)
 		{
 			m_moveRight = false;
 		}
-		if (t_event.key.code == sf::Keyboard::S || t_event.key.code == sf::Keyboard::Down)
+		if (t_event.key.code == sf::Keyboard::S)
 		{
 			m_moveDown = false;
 		}
+		if (t_event.key.code == sf::Keyboard::Q)
+		{
+			m_rotateLeft = false;
+		}
+		if (t_event.key.code == sf::Keyboard::E)
+		{
+			m_rotateRight = false;
+		}
 	}
-
 
 	if (controller.m_currentState.leftThumbStick.y >= 0 && controller.m_previousState.leftThumbStick.y < 0)
 	{
@@ -77,11 +98,26 @@ void InputManager::update(sf::Event& t_event)
 	{
 		m_moveLeft = false;
 	}
+
+	if (controller.m_currentState.leftB && !controller.m_previousState.leftB)
+	{
+		m_rotateLeft = false;
+	}
+
+	if (controller.m_currentState.rightB && !controller.m_previousState.rightB)
+	{
+		m_rotateRight = false;
+	}
+
+	if (playersYPos <= floor)
+	{
+		m_jumping = false;
+	}
 }
 
 InputManager::InputManager() : m_moveDown{ false },
 m_moveLeft{ false },
 m_moveRight{ false },
-m_moveUp{ false }
+m_moveUp{ false }, m_rotateLeft{ false }, m_rotateRight{ false }, m_jumping{ false }
 {
 }
